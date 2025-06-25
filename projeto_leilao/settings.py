ceb6ed0242d29fb73fec0dad92e7080b94c76f56
shellytoons <exeleo652@gmail.com>
2025-06-25 20:17:39 -0300

    Adicionando projeto completo

--- projeto_leilao/settings.py
+++ projeto_leilao/settings.py
@@ -34,6 +34,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
